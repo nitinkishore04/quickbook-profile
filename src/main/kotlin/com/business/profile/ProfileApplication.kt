26c0a4bf62ce3364b8021390a2dd3fbfd38c8196
Nitin <nitin.kishore04@gmail.com>
2023-09-03 16:02:02 +0530

    basic mongo and startup setup

--- src/main/kotlin/com/business/profile/ProfileApplication.kt
+++ src/main/kotlin/com/business/profile/ProfileApplication.kt
@@ -2,10 +2,12 @@ package com.business.profile
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 @SpringBootApplication
 class ProfileApplication
 
 fun main(args: Array<String>) {
+	TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"))
 	runApplication<ProfileApplication>(*args)
 }
