63ed91f0b74599601c795a175b00dce85e67679d
Nitin <nitin.kishore04@gmail.com>
2023-09-03 15:53:16 +0530

    code setup

---
+++ src/test/kotlin/com/business/profile/ProfileApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.business.profile
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
