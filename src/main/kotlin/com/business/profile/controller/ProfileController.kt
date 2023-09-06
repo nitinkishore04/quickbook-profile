c82e4e60e41b3155b1f3f1b6d9aa726ca341879e
Nitin <nitin.kishore04@gmail.com>
2023-09-06 18:10:45 +0530

    add cors access

--- src/main/kotlin/com/business/profile/controller/ProfileController.kt
+++ src/main/kotlin/com/business/profile/controller/ProfileController.kt
@@ -4,18 +4,14 @@ import com.business.profile.model.BusinessProfile
 import com.business.profile.service.ProfileService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/profile")
+@CrossOrigin(
+    origins = ["http://localhost:3000"],
+    methods = [RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE]
+)
 class ProfileController(private val service: ProfileService) {
 
     @GetMapping("/all")
